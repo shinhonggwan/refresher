292c737f1a608af7e5f4b4fb99b57b575dac1d8e
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2024-06-24 21:29:39 +0900

    greeting message

---
+++ exercises.py
@@ -0,0 +1,44 @@
+myValue = 999
+print("The type of myValue is ", type(myValue))
+myValue = "I am the very model of a modern major general."
+print("The type of myValue is ", type(myValue))
+
+dog_age = 8
+print("My dog is", dog_age, "years old.")
+print("My dog is " + str(dog_age) + " years old.")
+print(f"My dog is {dog_age} years old.")
+print("-------------------")
+pi = 3.14159
+print(pi)
+last_name = "Smith"
+print(last_name)
+length_in_inches = 19.3
+print(type(float(length_in_inches)))
+occupation = "haberdashery"
+print(type(str(occupation)))
+print("-------------------")
+floor_1 = 7 // 2
+print(floor_1)
+remainder = 13 % 6
+print(remainder)
+power1 = 2 ** 4
+print(power1)
+
+first_name = "David"
+last_name = "Cameron"
+full_name = first_name + " " + last_name
+print(full_name)
+print("-------------------")
+remainder1 = 19 % 6
+print(remainder1)
+power2 = 2 ** 4
+print(power2)
+conversion_ratio = 9 / 5
+print(conversion_ratio)
+num_planets = 9
+num_planets -= 1
+print(num_planets)
+title = "Doctor"
+last_name_2 = "Pierce"
+full_name_3 = title + " " + last_name_2
+print(full_name_3)
---
+++ mathexample.py
@@ -0,0 +1,3 @@
+num1 = 10
+num2 = 9
+print(num1 * num2)
