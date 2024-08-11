3a5aaab63a0ad1d0fb8cf3ce620d88891ab65f37
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2024-08-12 05:55:29 +0900

    Update 2

---
+++ factorial.py
@@ -0,0 +1,10 @@
+def sort_two_strings(string1, string2):
+    if string1.upper() < string2.upper():
+        return string1, string2
+    else:
+        return string2, string1
+
+print(sort_two_strings("aardvark", "Zebra"))
+print(sort_two_strings("Byron", "Lovelace"))
+
+
