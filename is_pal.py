0cb07c05bb5110cce17884e0fd31b3ac49e87a28
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2024-08-19 08:48:17 +0900

    Update 3

--- is_pal.py
+++ is_pal.py
@@ -13,3 +13,17 @@ print(hero[6:])
 print(hero[:])
 print(hero[::2])
 print(hero[::-1])
+
+print("-----------")
+"""A palindrome is a string that reads the same 
+forward or backward. Write a function called is_pal 
+that takes a string parameter and returns True if that 
+string is a palindrome, but returns False otherwise."""
+
+def is_plaindrome(word):
+    word = word.lower()
+
+    return word == word[::-1]
+
+print(is_plaindrome("hello"))
+print(is_plaindrome("civic"))
