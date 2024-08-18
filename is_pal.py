c8a5bace840183ad2a5fab150c0c2e2f062ccf8c
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2024-08-19 08:28:56 +0900

    Update 3

---
+++ is_pal.py
@@ -0,0 +1,15 @@
+#honggwan shin
+
+gem = "onyx"
+dino = "velociraptor"
+fruit = "strawberry"
+hero = "spiderman"
+poem = "mary had a little lamb"
+
+
+print(fruit[2:5])
+print(hero[:6])
+print(hero[6:])
+print(hero[:])
+print(hero[::2])
+print(hero[::-1])
