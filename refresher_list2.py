f3dec1b9d3c8176ff5dfa7073e619ab8608a6414
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2024-08-20 08:51:40 +0900

    Update 3

---
+++ refresher_list2.py
@@ -0,0 +1,8 @@
+odds = [7, 5, 9, 1, 13, 11, 3]
+evens = [8, 4, 10, 6, 2]
+palindromes = ["hannah", "tacocat", "bob", "mom", "dad"]
+
+print(min(odds))
+print(max(palindromes))
+print(evens.sort())
+print(palindromes.sort())
