44312d7fbaa00982256655786c248210ad161f0c
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2024-08-28 07:45:54 +0900

    Figure it out

--- refresher_list4_exercises.py
+++ refresher_list4_exercises.py
@@ -8,5 +8,11 @@ print(every_other(list1))
 
 def array_sum(list):
     # we need them in one word and count
+    counter = 0
+    for i in list:
+        counter += len(i)
+    return counter
 
 list2 = ["Wayne", "Katie","Daryl", "Don"]
+
+print(array_sum(list2))
