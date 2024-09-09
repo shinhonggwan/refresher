67466a19e0897e75f6c932e8196999ba36e6f4c5
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2024-09-10 08:51:25 +0900

    no comment

---
+++ tuple_1.py
@@ -0,0 +1,13 @@
+some_tuple = (1, 2, 3)
+num_1, num_2, num_3 = some_tuple
+print(num_1)
+
+print("---------")
+
+def insert_front(list, something):
+    # list.append(something)
+    # need to insert something to the front of the list
+
+    return list
+
+print(insert_front([9, -55, 37], "bob"))
