eaa4de51e4d8dd6546908554a3917e22d5d87cd8
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2024-09-11 08:49:21 +0900

    no comment

--- tuple_1.py
+++ tuple_1.py
@@ -11,3 +11,11 @@ def insert_front(list, something):
     return new_answer
 
 print(insert_front([9, -55, 37], "bob"))
+
+print("---------")
+
+def delete_last(list):
+    list = list[:-1]
+    return list
+
+print(delete_last([7, "joe", "apple", 9.81, False]))
