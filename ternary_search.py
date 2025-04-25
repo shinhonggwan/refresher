6391b2a1ecc74624877a3d2b05bc3649cb251ebb
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-04-25 15:13:56 +0900

    CS 325 Assignment 2

--- ternary_search.py
+++ ternary_search.py
@@ -3,6 +3,7 @@
 # Course: CS 325
 # Assignment: Assignment 3 Correctness of Algorithms and Divide & Conquer
 
+
 def ternary_search(input_list, target):
 
     def search_function(left, right):
@@ -11,10 +12,26 @@ def ternary_search(input_list, target):
             return None
 
         # Thie effectivly breaks the array into three parts
-        middle_index_1 = ( (right - left) + left ) // 3
+        middle_index_1 = ( left + (right - left) ) // 3
         middle_index_2 = ( right - (right - left) ) // 3
 
-    return search_function()
+        if input_list[middle_index_1] == target:
+            return middle_index_1 + 1
+        if input_list[middle_index_2] == target:
+            return middle_index_2 + 1
+
+        if target < input_list[middle_index_1]:
+            return search_function(left, middle_index_1 - 1)
+        elif target > input_list[middle_index_2]:
+            return search_function(middle_index_2 + 1, right)
+        else:
+            return search_function(middle_index_1 + 1, middle_index_2 - 1)
+
+    return search_function(0, len(input_list) - 1)
+
+
+
+print(ternary_search([10, 20, 30, 40, 50], 40))
 
 
 
