ce4e8603f1f5087a4869d42e3ae5537e2a22d4f0
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-04-25 15:00:45 +0900

    CS 325 Assignment 2

--- ternary_search.py
+++ ternary_search.py
@@ -2,3 +2,12 @@
 # Date: April 24, 2025 Oregon Time
 # Course: CS 325
 # Assignment: Assignment 3 Correctness of Algorithms and Divide & Conquer
+
+def ternary_serach(input_list, target):
+
+    def search_function(left, right):
+        if left > right:
+            # This is the base case
+            return None
+
+
