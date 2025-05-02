ec822382fb86e3d9c83a0ab142473bc41c7cef42
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-05-02 13:55:17 +0900

    CS 325 Assignment 4

--- MaxSet.py
+++ MaxSet.py
@@ -2,3 +2,11 @@
 # Date: May 1, 2025 Oregon Time
 # Course: CS 325
 # Assignment: Assignment 4: Dynamic Programming
+
+def max_independent_set(nums):
+
+    if all(x < 0 for x in nums) or not nums:
+        # returns empty if the numbers from the list are all negative or no num in the list
+        return []
+
+    length_num = len(nums)
