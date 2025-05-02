04715b5e31df9f7a45914106102ec3d86ca4a2d0
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-05-02 12:33:23 +0900

    CS 325 Assignment 2

--- DNAMatch.py
+++ DNAMatch.py
@@ -11,4 +11,16 @@ def dna_match_topdown(DNA1, DNA2):
             # base case
             return 0
 
+        if DNA1[x] == DNA2[y]:
+            # if the characters line up together, include it and move to the ext one
+            return 1 + subsequence(x + 1, y + 1)
 
+        else:
+            # if they dont, then skip a character either in DNA1 or DNA2
+            return max(subsequence(x + 1, y), subsequence(x, y +1))
+
+    return subsequence(0, 0)
+    # recursion from the start of both characters
+
+
+print(dna_match_topdown("TAGTTCCGTCAAA" "GTGTTCCCGTCAAA"))
