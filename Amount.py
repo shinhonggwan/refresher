2f1761bf52aa6b998017cf0bae8d1466fe0616b7
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-05-09 14:00:17 +0900

    CS 325 Assignment 5

--- Amount.py
+++ Amount.py
@@ -3,9 +3,35 @@
 # Course: CS 325
 # Assignment: Assignment: Backtracking
 
+
+
+def permutations_backtracker(starting_index, combination, remainder_sum, unique_numbers, counter, results):
+    if remainder_sum == 0:
+        # if the remaining sum is 0, the combination is valid thus add it to the result
+        results.append(combination[:])
+        return
+    if remainder_sum < 0:
+        # if the remaining sum is below 0, then stop
+        return
+
+
+
 def amount(nums_list, target_sum):
     counter = {}
     # a dictionary that counts how many times
     for number in nums_list:
         if number in counter:
-            counter[num]
+            counter[number] += 1
+        else:
+            counter[number] = 1
+
+    unique_numbers = sorted(counter.keys())
+    results = []
+
+    permutations_backtracker(0, [], target_sum, unique_numbers, counter, results)
+    return results
+
+# nums_list = [11, 1, 3, 2, 6, 1, 5]
+# target_sum = 8
+# print(amount(nums_list, target_sum))
+# wtf...
