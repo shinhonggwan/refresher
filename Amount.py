3b69eb42e712dc2eaea6a70acfae603dc9f07ac1
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-05-09 14:35:20 +0900

    CS 325 Assignment 5

--- Amount.py
+++ Amount.py
@@ -4,21 +4,33 @@
 # Assignment: Assignment: Backtracking
 
 
-
 def permutations_backtracker(starting_index, combination, remainder_sum, unique_numbers, counter, results):
     if remainder_sum == 0:
-        # if the remaining sum is 0, the combination is valid thus add it to the result
-        results.append(combination[:])
+        # base case
+        # if the remaining sum is 0, the combination is valid thus copy, add it to the result
+        results.append(combination.copy())
         return
     if remainder_sum < 0:
-        # if the remaining sum is below 0, then stop
+        # if the remaining sum is below 0, then stop exploring
         return
 
+    for i in range(starting_index, len(unique_numbers)):
+        # iterate from start till the end
+        number = unique_numbers[i]
+        if counter[number] > 0:
+            # if the number hasn't been used, then pick this number, add it to combination, and minus one to the oucnter
+            combination.append(number)
+            counter[number] -= 1
 
+            permutations_backtracker(i, combination, remainder_sum - number, unique_numbers, counter, results)
+            # recursion. only use the smae number if there is other possible options
+            combination.pop()
+            # get rid of the last number and try another combination
+            counter[number] += 1
 
 def amount(nums_list, target_sum):
     counter = {}
-    # a dictionary that counts how many times
+    # a dictionary that counts how many times each number take place
     for number in nums_list:
         if number in counter:
             counter[number] += 1
@@ -26,12 +38,13 @@ def amount(nums_list, target_sum):
             counter[number] = 1
 
     unique_numbers = sorted(counter.keys())
+    # sorts list of unique numbers and no same combination
     results = []
 
     permutations_backtracker(0, [], target_sum, unique_numbers, counter, results)
+    # backtracker starting from 0
     return results
 
-# nums_list = [11, 1, 3, 2, 6, 1, 5]
-# target_sum = 8
-# print(amount(nums_list, target_sum))
-# wtf...
+nums_list = [11, 1, 3, 2, 6, 1, 5]
+target_sum = 8
+print(amount(nums_list, target_sum))
