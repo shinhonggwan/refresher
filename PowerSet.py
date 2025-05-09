28a103c61ed46240897592a4570fdabefddc4f47
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-05-09 11:21:57 +0900

    CS 325 Assignment 5

--- PowerSet.py
+++ PowerSet.py
@@ -2,3 +2,26 @@
 # Date: May 7, 2025 Oregon Time
 # Course: CS 325
 # Assignment: Assignment: Backtracking
+
+from copy import deepcopy
+
+def combination_sum_helper(nums, start, result, remainder, combination):
+
+    if(remainder == 0):
+        result.append(deepcopy(combination))
+        return
+    elif( remainder <0):
+        return # sum exceeded the target
+    for i in range(start, len(nums)):
+        combination.append(nums[i])
+        combination_sum_helper(nums, i, result, remainder-nums[i], combination)
+        #backtrack
+        combination.pop()
+
+
+def combination_sum(nums, target):
+    result = []
+    combination_sum_helper(nums,0, result, target,[])
+    print(result)
+
+print(combination_sum([2,3,6,7], 7 ))
