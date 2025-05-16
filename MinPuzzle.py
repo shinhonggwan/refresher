f18a278246d9d9620aca3880a450c6f60f8cb815
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-05-16 14:08:51 +0900

    CS 325 Assignment 6

--- MinPuzzle.py
+++ MinPuzzle.py
@@ -7,4 +7,11 @@ def minEffort(puzzle):
     # the goal is to reach the bottom-right cell '[m-1][n-1]' with minimal effort.
 
     m, n = len(puzzle), len(puzzle[0])
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    # up, down, left ,right
+
+    # Step 1: Initialize distances and visited nodes
+    # can use a priority queue here
+    dist = [[float('infinity')] * n for _ in range(m)]
+    dist[0][0] = 0
 
