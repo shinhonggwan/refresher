b065dcaca8940f51b546aae899df1a8898d4d21d
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-05-16 13:52:36 +0900

    CS 325 Assignment 6

--- MinPuzzle.py
+++ MinPuzzle.py
@@ -4,4 +4,7 @@
 # Assignment: Assignment: Graph Algorithms 1
 
 def minEffort(puzzle):
-    return
+    # the goal is to reach the bottom-right cell '[m-1][n-1]' with minimal effort.
+
+    m, n = len(puzzle), len(puzzle[0])
+
