3db49a0f46bfae5f10c9035cf4b62f1e3620f17b
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-05-23 12:49:36 +0900

    CS 325 Assignment Greedy Technique

--- FeedDog.py
+++ FeedDog.py
@@ -3,3 +3,17 @@
 # Course: CS 325
 # Assignment: Assignment: Greedy Technique & Graph Algorithms Part-2
 
+def feedDog(hunger_level, biscuit_size):
+    # a dog feeding function that tells how many dogs can satisfy with biscuits
+    result = []
+
+    hunger = 0
+    biscuit = 0
+
+    n = len(hunger_level)
+    m = len(biscuit_size)
+
+
+
+    while hunger < n and biscuit < m:
+        if
