f9badc3357fc3a0eae17890716011e64a3c6bc5c
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-05-23 15:19:15 +0900

    CS 325 Assignment Greedy Technique

--- MST.py
+++ MST.py
@@ -8,27 +8,33 @@ def connect_bridges(G):
     # This produces a list of tubles of two islands and the cost of the bridge between them
 
     number_of_islands = len(G)
+    # total number of islands
     distance = []
     previous_node = []
 
     for i in range(number_of_islands):
+        # initialize distance and previou node arrays
         distance.append(float('inf'))
         previous_node.append(None)
 
     visited = set()
+    # track visited nodes
     MST = []
+    # store MST edges
 
     distance[0] = 0
+    # starting from island 0
 
     while len(visited) < number_of_islands:
-        # Step 1: Find the unvisited node with the smallest distance
+        # Step 1: Find the unvisited node with the smallest distance until visiting all islands
         currentNode = None
         for v in range(number_of_islands):
             if v not in visited and (currentNode is None or distance[v] < distance[currentNode]):
                 currentNode = v
 
         if currentNode is None:
-            break # for disconnected graph
+            # if there is no unvisited node remaining
+            break
 
         visited.add(currentNode)
 
@@ -38,8 +44,11 @@ def connect_bridges(G):
 
         # Step 2: Update distances for neighbors
         for neighbor in range(number_of_islands):
+            # iteration through all possible neighboring islands
             edge_weight = G[currentNode][neighbor]
+            # retrieve weight of cost of the edge
             if neighbor not in visited and edge_weight != 0 and edge_weight < distance[neighbor]:
+                # if neighbor not in visited and there is no actual bridge between currentnode and neighbor and the currentnode cost is lower than the previous
                 distance[neighbor] = edge_weight
                 previous_node[neighbor] = currentNode
 
