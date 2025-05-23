93f8acd959a5801978874f6d342b359832975f15
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-05-23 14:52:24 +0900

    CS 325 Assignment Greedy Technique

--- MST.py
+++ MST.py
@@ -3,8 +3,13 @@
 # Course: CS 325
 # Assignment: Assignment: Greedy Technique & Graph Algorithms Part-2
 
-# The below code is modified from the Exploration 7.3 Minimum Spanning Tree - Prim's Algorithm
 def connect_bridges(G):
+    # This took The Exploration 7.3 Minimum Spanning Tree - Prim's Algorithm work/exmple.py as an example.
     # This produces a list of tubles of two islands and the cost of the bridge between them
-    return
 
+    number_of_islands = len(G)
+    distance = [float('inf')] * number_of_islands
+    previous_node = [None] * number_of_islands
+
+    visited = set()
+    MST = []
