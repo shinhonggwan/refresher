0ba25560306b2db0d963280ed317b850489c3245
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-05-23 13:04:09 +0900

    CS 325 Assignment Greedy Technique

--- FeedDog.py
+++ FeedDog.py
@@ -14,17 +14,15 @@ def feedDog(hunger_level, biscuit_size):
     hungry = 0
     biscuit = 0
 
-    sorted_hunger_level = hunger_level.sort()
-    sorted_biscuit_size = biscuit_size.sort()
+    sorted_hunger_level = sorted(hunger_level)
+    sorted_biscuit_size = sorted(biscuit_size)
 
     while hungry < n and biscuit < m:
         if sorted_biscuit_size[biscuit] >= sorted_hunger_level[hungry]:
-
             hungry += 1
         biscuit += 1
 
     return hungry
-
 
 
 print(feedDog([1, 2, 3], [1, 1]))
