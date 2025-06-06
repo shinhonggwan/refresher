bce21e773099450179d6209c9cdcba27f2bdc874
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-06-06 15:57:54 +0900

    CS 325 Assignment: Heuristic Algorithms

--- TSP.py
+++ TSP.py
@@ -9,24 +9,29 @@
 
 def solve_tsp(G):
     # traveling salesman problem (TSP)
-    # Using MST-based heuristic
-    # returning a list of node indices indicating the TSP tour path
+    # Using Nearest Neighbor Heuristic
+    # somehow using MST based heuristic did not work...
 
     number_of_nodes = len(G)
     #total number of nodes in the graph
-
     selected = [False] * number_of_nodes
     # track selected nodes
     tour = [0]
+    # start from node -
     current = 0
+    # current node
     selected[current] = True
+    # record visited to the starting node
 
     while len(tour) < number_of_nodes:
-        nearest = None
+        # while loop until all node is visited
+        nearest = -1
+        # nearest unvisited index tracker
         minimum_distance = float('inf')
+        # record the found minimum distance
 
         for i in range(number_of_nodes):
-            # finding the nearrest unvisited neighbor
+            # finding the nearest unvisited neighbor
             if G[current][i] > 0 and not selected[i]:
                 # iteration, find the unvisited
                 if G[current][i] < minimum_distance:
