1f910b38f39ac833417ccd42290bf3eaa5518a5b
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-06-06 14:58:55 +0900

    CS 325 Assignment: Heuristic Algorithms

--- TSP.py
+++ TSP.py
@@ -8,53 +8,76 @@
 # from the Exploration 9.3 - Heuristic Algorithms for Traveling Salesman Problem
 
 def solve_tsp(G):
-    # traveling salesman problem
-    # returning a list of indices indicating the path taken
+    # traveling salesman problem (TSP)
+    # Using MST-based heuristic
+    # returning a list of node indices indicating the TSP tour path
 
     number_of_nodes = len(G)
+    #total number of nodes in the graph
 
     MST = [[] for _ in range(number_of_nodes)]
+    # MST as an adjacency list
     selected = [False] * number_of_nodes
+    # track selected nodes
     key = [float('inf')] * number_of_nodes
+    # store edge that connects
     parent = [-1] * number_of_nodes
+    # store parent node in the MST
     key[0] = 0
+    # start from node 0
 
-    for _ in range(n):
+    for _ in range(number_of_nodes):
+        # Finding the univisited node with lowest key value using Prim's Algorithm
         unvisited = -1
         minimum_key = float('inf')
         for i in range(number_of_nodes):
+            # iteration, find the unvisiteed with minimum key
             if not selected[i] and key[i] < minimum_key:
                 minimum_key = key[i]
                 unvisited = i
 
         if unvisited == -1:
+            # if there is no more nodes to visit, then stop
             break
 
         selected[unvisited] = True
+        # record selected node for the MST
 
         for i in range(number_of_nodes):
+            # updating key and parent for adjacent nodes
             if G[unvisited][i] < key[i] and G[unvisited][i] > 0 and not selected[i]:
+                # if node i hasn't been selected and weidhts is greater than 0
                 key[i] = G[unvisited][i]
                 parent[i] = unvisited
+                # parent of i i = current node in MST
 
-    for i in range(1, n):
-        # make the MST as an adjacency list
+    for i in range(1, number_of_nodes):
+        # convert the MST array to as an adjacency list
         if parent[i] != -1:
             MST[parent[i]].append(i)
+            # append i as a neighbor of its parent
             MST[i].append(parent[i])
+            # append parent as a neighbor of i
 
-    def dfs(unvisited, visited_nodes, tour):
-        # performing DFS traversal on the MST
+    def DFS(unvisited, visited_nodes, tour):
+        # creating TSP path by performing DFS traversal on the MST
         visited_nodes[unvisited] = True
         tour.append(unvisited)
+        # append univisited node to tour path
         for i in MST[unvisited]:
+            # for loop to visit all unvisited neighbors
             if not visited_nodes[i]:
-                dfs(i, visited_nodes, tour)
+                DFS(i, visited_nodes, tour)
+                # recursion
 
     visited_nodes = [False] * number_of_nodes
+    # record visited nodes while DFS operation
     tour = []
-    dfs(0, visited_nodes, tour)
+    # TSP Tour result
+    DFS(0, visited_nodes, tour)
+    # DFS operation from node 0
     tour.append(0)
+    # go back to 0 starting node
 
     return tour
 
