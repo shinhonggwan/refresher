626ecdce74d4e8f3203702efdafe625dd1e4c856
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2025-06-06 13:40:08 +0900

    CS 325 Assignment: Heuristic Algorithms

--- TSP.py
+++ TSP.py
@@ -2,3 +2,12 @@
 # Date: June 5, 2025 Oregon Time
 # Course: CS 325
 # Assignment: Assignment: Heuristic Algorithms
+
+# The below function took note of the pseudocode as well as the
+# work/solution.py from Exploration 9.2 - Heuristic Algorithms for NP-Hard Problems
+
+def solve_tsp(G):
+    # traveling salesman problem
+    # returning a list of indices indicating the path taken
+
+    solution = [0]
